e5f01e16aa6ff06d5b54799ab5101773001f3f69
anthony <diomede1991@libero.it>
2022-04-17 12:15:13 +0200

    non ho capito l'esercizio e mo faccio come me pare

--- main/index.php
+++ main/index.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php_dischi</title>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 <body>
-    
+<?php include __DIR__.'/../element/API.php' ?>
 
-    <script src="script.js"></script>
+
 </body>
 </html>
---
+++ main/script.js
@@ -0,0 +1,9 @@
+const { default: axios } = require("axios");
+
+const app = new Vue({
+    el : "#root";
+    data() {
+        Array : [];
+    },
+
+})
